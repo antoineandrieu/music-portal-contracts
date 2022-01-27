27c7d20fd70a10160655663e36e5a67918eb1e14
Coding Ant <antoine@codingant.io>
2022-01-27 09:14:17 +0100

    feat: add base program

--- programs/music-portal-contracts/src/lib.rs
+++ programs/music-portal-contracts/src/lib.rs
@@ -6,9 +6,26 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod music_portal_contracts {
     use super::*;
     pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+        // Get a reference to the account
+        let base_account = &mut ctx.accounts.base_account;
+        // Initialize total_gifs
+        base_account.total_songs = 0;
         Ok(())
     }
 }
 
+// Attach variables to the context
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+// Tell Solana what we want to store on this account
+#[account]
+pub struct BaseAccount {
+    pub total_songs: u64,
+}
--- tests/music-portal-contracts.js
+++ tests/music-portal-contracts.js
@@ -1,14 +1,36 @@
 const anchor = require('@project-serum/anchor');
 
-describe('music-portal-contracts', () => {
+const { SystemProgram } = anchor.web3;
 
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+describe('music-portal-contracts', () => {
+  // Create and set the provider
+  // We set it before but we needed to update it, so that it can communicate with our frontend
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
 
   it('Is initialized!', async () => {
     // Add your test here.
     const program = anchor.workspace.MusicPortalContracts;
-    const tx = await program.rpc.initialize();
-    console.log("Your transaction signature", tx);
+
+    // Create an account keypair for our program to use.
+    const baseAccount = anchor.web3.Keypair.generate();
+
+    // Call initialize, pass it the params it needs
+    let tx = await program.rpc.initialize({
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [baseAccount],
+    });
+
+    console.log('Your transaction signature', tx);
+
+    // Fetch data from the account.
+    let account = await program.account.baseAccount.fetch(
+      baseAccount.publicKey
+    );
+    console.log('👀 Song Count', account.totalSongs.toString());
   });
 });
